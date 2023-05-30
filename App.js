a8cfa2af29049e68351eebc7c5f490848af9571e
Daphne-Holy <113779719+Daphne-Holy@users.noreply.github.com>
2023-05-30 10:43:41 +0700

    App.js

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
+
 import { StyleSheet, Text, View } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import Home from './src/widgets/home';
+import CalendarFirstWeek from './src/widgets/calendar';
 
 export default function App() {
 
   const Tab = createBottomTabNavigator();
   return (
+    <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Home" component={Home}/>
+        <Tab.Screen name="Calendar" component={CalendarFirstWeek}/>
       </Tab.Navigator>
+      </NavigationContainer>
   );
 }
 
