730f62c2e853f691cdb7eea16fc9f98c658a0708
Daphne-Holy <113779719+Daphne-Holy@users.noreply.github.com>
2023-05-30 10:45:07 +0700

    button.js

--- src/widgets/button.js
+++ src/widgets/button.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 const Button = () => {
-    return (
-        <View>
-            <View style={{flexDirection:"row", alignItems:"center", padding:10}}>
-                <FontAwesome5 name={"home"} color="black" size={30}/>
-                
+  const menulist = [
+    { id: 1, name: "Cafe", icon: "store" },
+    { id: 2, name: "Homestay", icon: "home" },
+    { id: 3, name: "Camping", icon: "campground" },
+    { id: 4, name: "Photo Spot", icon: "camera" },
+  ];
 
-            </View>
-        </View>
-    );
-}
+  return (
+    <View>
+      <View style={{ flexDirection: "row", flexWrap: "wrap" }}>
+        {menulist.map((v, index) => (
+          <MenuItem item={v} key={index} />
+        ))}
+      </View>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const MenuItem = ({ item }) => {
+  return (
+    <View style={styles.btn_item}>
+      <View style={styles.btn_item}>
+        <FontAwesome5 name={item.icon} size={35} color={"green"} />
+      </View>
+      <Text styles={styles.btn_text}>{item.name}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  btn_item: {
+    width: 100,
+    marginHorizontal: 5,
+    marginVertical: 10,
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn_icon: { marginBottom: 10 },
+  btn_text: { color: "black", fontSize: 14 },
+});
 
 export default Button;
