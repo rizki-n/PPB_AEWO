d181f6e67da4385e6717b9ba217cedeca4f93799
Daphne-Holy <113779719+Daphne-Holy@users.noreply.github.com>
2023-06-17 10:29:31 +0700

    home.js

--- src/widgets/home.js
+++ src/widgets/home.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import {View, StyleSheet, StatusBar} from 'react-native';
-
+import {View, StyleSheet, StatusBar, SafeAreaView, Text} from 'react-native';
+import Button from './button';
+import Header from './header';
 
 class Home extends Component {
     render() {
         return (
             
             <SafeAreaView style={{ flex:1, backgroundColor: "white"}}>
+            <Header/>
 
-
-            <View style={{flex:10, backgroundColor:"green"}}>
-                <StatusBar hidden={false}/>
+            <View style={{flex:10, backgroundColor:"lightgray"}}>
                 
+                <StatusBar hidden={false}/>
+                <Text style={styles.subtxt}>
+                Tempatnya berwisata sambil belajar dengan{'\n'}harga terjangkau.
+                </Text>
+                <Text style={styles.txt}>
+                    Fasilitas Yang Tersedia
+                </Text>
+                <Button/>
+                <Text style={styles.txt}>
+                    Overview
+                </Text>
             </View>
 
             </SafeAreaView>
@@ -20,6 +31,18 @@ class Home extends Component {
     }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+    txt:{
+        fontSize: 20,
+        fontWeight:"bold",
+        paddingHorizontal: 10
+    },
+    subtxt:{
+        paddingHorizontal: 10,
+        marginVertical: 10
+    }
+
+})
 
 export default Home;
